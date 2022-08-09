264e10ab3c2670a95f516fa1a5bf91ff0bb5e65a
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-08-09 12:22:26 +0530

    Running sum

---
+++ src/com/practice/RunningSum.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class RunningSum {
+
+    public int[] runningSum(int[] nums) {
+
+        for(int i=1; i<nums.length; i++) nums[i] += nums[i-1];
+        return nums;
+    }
+}
+
+
+/*
+ [1,2,3,4]----input
+
+ [1,3,6,10]<---output
+ */
